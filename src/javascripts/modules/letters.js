1c9f03e2be7d1352e5b9e64c364afec737036611
tommers-walker <tommers.walker@gmail.com>
2019-01-04 15:37:27 +0000

    Experimenting with moving shadow

--- src/javascripts/modules/letters.js
+++ src/javascripts/modules/letters.js
@@ -2,24 +2,44 @@ import 'gsap';
 
 const letterEls = Array.from(document.querySelectorAll('.js-letter'));
 
-const letters = {
-  init() {
-    const tl = new TimelineMax()
+const midX = window.innerWidth/2;
+const midY = window.innerHeight/2;
 
-    tl.staggerFromTo(letterEls, 0.2,
-      {
-        opacity: 0,
-        scale: 0.95,
-        textShadow: "0 0 0 rgba(0,0,0,0.2)",
-      },
-      {
-        opacity: 1,
-        scale: 1,
-        textShadow: "0.5vw 0 1.25vw rgba(0,0,0,0.1)",
-        ease: Back.easeOut.config(1.25)
-      },
-      0.05
-    )
+const letters = {
+  shadow(e) {
+    let x;
+    let y;
+    let blur;
+
+    x = -(e.x - midX);
+    y = -(e.y - midY);
+
+    blur = 1.25 * Math.abs(x*0.001)+Math.abs(y*0.001);
+
+    TweenLite.set(letterEls, {textShadow: `${x*0.01}px ${y*0.01}px 1.25vw rgba(0,0,0,0.1)`});
+  },
+
+  init() {
+    // const tl = new TimelineMax()
+    //
+    // tl.staggerFromTo(letterEls, 0.2,
+    //   {
+    //     opacity: 0,
+    //     scale: 0.95,
+    //     textShadow: "0 0 0 rgba(0,0,0,0.2)",
+    //   },
+    //   {
+    //     opacity: 1,
+    //     scale: 1,
+    //     textShadow: "0.5vw 0 1.25vw rgba(0,0,0,0.1)",
+    //     ease: Back.easeOut.config(1.25)
+    //   },
+    //   0.05
+    // );
+
+    document.addEventListener('mousemove', function(e) {
+      letters.shadow(e);
+    })
   }
 }
 
